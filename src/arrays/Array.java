db96e8508b669721e9dfe2c1843f1e5d66b8d5df
zakaria <zakariabaouali@outlook.com>
2024-10-09 07:57:22 +0100

    implement deleteBy index method , and find with two pointers

--- src/arrays/Array.java
+++ src/arrays/Array.java
@@ -16,6 +16,41 @@ public class Array<T> {
         this.items[this.count++] = item;
     }
 
+    public void deleteAt(int index) {
+        if (this.Empty())
+            return;
+
+        for (int i = index; i < this.count; i++) {
+            this.items[i] = this.items[i + 1];
+        }
+        this.count--;
+    }
+
+    public int indexOf(T item) {
+        for (int i = 0; i < this.count; i++) {
+            if (this.items[i] == item)
+                return i;
+        }
+        return -1;
+    }
+
+    // use two pointers
+    public int indexOfWithTwoPointers(T item) {
+        int start = 0;
+        int end = this.count - 1;
+        while (end >= start) {
+            if (this.items[start] == item)
+                return start;
+            else if (this.items[end] == item)
+                return end;
+
+            start++;
+            end--;
+
+        }
+        return -1;
+    }
+
     private void checkCapacity() {
         if (this.count == this.items.length) {
             Object[] newItems = new Object[this.count * 2];
@@ -24,6 +59,10 @@ public class Array<T> {
             }
             this.items = newItems;
         }
+    }
+
+    private boolean Empty() {
+        return this.count < 0;
     }
 
     @Override
--- src/arrays/Main.java
+++ src/arrays/Main.java
@@ -9,7 +9,11 @@ public class Main {
         items.insert(20);
         items.insert(30);
         items.insert(40);
+        items.insert(50);
 
-        System.out.println(items);
+        // items.deleteAt(1);
+        System.out.println("index : " + items.indexOfWithTwoPointers(30));
+
+        // System.out.println(items);
     }
 }
